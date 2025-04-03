08c0776e6a677bb731378ee01f881462fe1b4fdd
Austin F <93173601+msudawg07@users.noreply.github.com>
2025-04-03 07:51:26 -0500

    Create dog.js

---
+++ public/dog.js
@@ -0,0 +1,37 @@
+export const dogs = {
+  "r1f_ll5VX": {
+    "url": "https://cdn2.thedogapi.com/images/r1f_ll5VX_1280.jpg",
+    "likes": 0,
+    "dislikes": 0,
+    "comments": [],
+    "num": 1
+  },
+  "h0vltXvfC": {
+    "url": "https://cdn2.thedogapi.com/images/h0vltXvfC.jpg",
+    "likes": 0,
+    "dislikes": 0,
+    "comments": [],
+    "num": 2
+  },
+  "z4xMdelMo": {
+    "url": "https://cdn2.thedogapi.com/images/z4xMdelMo.jpg",
+    "likes": 0,
+    "dislikes": 0,
+    "comments": [],
+    "num": 3
+  },
+  "NCA5enafW": {
+    "url": "https://cdn2.thedogapi.com/images/NCA5enafW.jpg",
+    "likes": 0,
+    "dislikes": 0,
+    "comments": [],
+    "num": 4
+  },
+  "-Lbz7P9jl" : {
+    "url": "https://cdn2.thedogapi.com/images/-Lbz7P9jl.jpg",
+    "likes": 0,
+    "dislikes": 0,
+    "comments": [],
+    "num": 5
+  }
+}
